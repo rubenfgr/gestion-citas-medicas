573b70096a056a35387f536811ad4c19b8c6f7e5
rubenfgr <rubenfgr87@outlook.com>
2021-07-08 10:19:55 +0200

    client entity defined

--- backend/src/clients/entities/client.entity.ts
+++ backend/src/clients/entities/client.entity.ts
@@ -1,9 +1,26 @@
-import { Column, OneToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './../../users/entities/user.entity';
 
 export class Client {
-    @PrimaryGeneratedColumn('increment')
-    id: number;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
 
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
 
+  @Column({ type: 'varchar', unique: true })
+  cif: string;
 
+  @Column({ type: 'varchar', unique: true })
+  razonSocial: string;
+
+  @Column({ type: 'varchar' })
+  direccion: string;
+
+  @Column({ type: 'varchar' })
+  municipio: string;
+
+  @Column({ type: 'varchar' })
+  provincia: string;
 }
